92ee303be60e81f9a83b4822f598483ed83e62e7
TianFei <3040247575@qq.com>
2022-09-20 20:45:38 +0800

    docs(custom): [C设置跨域

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,11 +9,23 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { addMapping } from "_@jridgewell_gen-mapping@0.3.2@@jridgewell/gen-mapping";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      a:"admin",
+      b:"admin",
+    }
+  },
   components: {
     HelloWorld,
   },
+  created(){
+  this.$axios.post('/api/login',{username:this.a,password:this.b}).then(res=>{
+    console.log(res);
+  })
+  }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,24 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
+    // 端口号
     port:8888,
+    // 主机名
     host:"localhost",
+    // 启动项目时自动打开浏览器
     open:false,
-    https:false
+    // 是否开启https
+    https:false,
+    // 配置跨域
+    proxy:{
+      "/api":{
+        target:"http://ceshi5.dishait.cn/admin",
+        changOrigin:true,
+        pathRewrite:{
+          "^/api":""
+        }
+      }
+    }
   },
   lintOnSave:false
 });

6e782e889b9030f7fcc978d197d8b22a5d3d8e63
TianFei <3040247575@qq.com>
2022-09-20 16:47:32 +0800

    feat(custom): 配serve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer:{
+    port:8888,
+    host:"localhost",
+    open:false,
+    https:false
+  }
 });
